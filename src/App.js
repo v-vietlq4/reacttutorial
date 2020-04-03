cf54d8a648cc272c01892a4d69696fed7ec6b0d4
vietawake <vietawake@gmail.com>
2020-04-03 14:09:40 +0700

    Update Hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Person from "./Person/Person";
+const app = props => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [personState, setPersonState] = useState({
+    persons: [
+      { name: "Vietawake", age: 30 },
+      { name: "Quangawake", age: 22 },
+      { name: "Khuongawake", age: 22 }
+    ]
+  });
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [otherState, setOtherState] = useState('some other state');
 
-function App() {
+  console.log(personState, otherState);
+
+  const swichNameHanler = () => {
+    setPersonState({
+      persons: [
+        { name: "Viet", age: 31 },
+        { name: "Quang", age: 23 },
+        { name: "Khuong", age: 22 }
+      ]
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,9 +37,25 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={swichNameHanler}>Swich name</button>
+        <Person name="Viet" age="21">
+          My hobbies: Reading Books{" "}
+        </Person>
+        <Person
+          name={personState.persons[0].name}
+          age={personState.persons[0].age}
+        />
+        <Person
+          name={personState.persons[1].name}
+          age={personState.persons[1].age}
+        />
+        <Person
+          name={personState.persons[2].name}
+          age={personState.persons[2].age}
+        />
       </header>
     </div>
   );
-}
+};
 
-export default App;
+export default app;
