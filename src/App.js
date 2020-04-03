9dbd26d9d07d3f1857b75c4cdf9e28ee8cd88a4b
vietawake <vietawake@gmail.com>
2020-04-03 16:02:45 +0700

    Add styling and pass data

--- src/App.js
+++ src/App.js
@@ -1,61 +1,74 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Person from "./Person/Person";
-const app = props => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [personState, setPersonState] = useState({
-    persons: [
-      { name: "Vietawake", age: 30 },
-      { name: "Quangawake", age: 22 },
-      { name: "Khuongawake", age: 22 }
-    ]
-  });
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [otherState, setOtherState] = useState('some other state');
-
-  console.log(personState, otherState);
-
-  const swichNameHanler = () => {
-    setPersonState({
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       persons: [
-        { name: "Viet", age: 31 },
-        { name: "Quang", age: 23 },
-        { name: "Khuong", age: 22 }
+        { name: 'Vietawake', age: 30 },
+        { name: 'Quangawake', age: 22 },
+        { name: 'Khuongawake', age: 22 }
+      ]
+    };
+  }
+
+  swichNameHanler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 31 },
+        { name: 'Quang', age: 23 },
+        { name: 'Khuong', age: 22 }
       ]
     });
-  };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={swichNameHanler}>Swich name</button>
-        <Person name="Viet" age="21">
-          My hobbies: Reading Books{" "}
-        </Person>
-        <Person
-          name={personState.persons[0].name}
-          age={personState.persons[0].age}
-        />
-        <Person
-          name={personState.persons[1].name}
-          age={personState.persons[1].age}
-        />
-        <Person
-          name={personState.persons[2].name}
-          age={personState.persons[2].age}
-        />
-      </header>
-    </div>
-  );
+  }
+
+  nameChangeHanler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'newNam', age: 31 },
+        { name: event.target.value, age: 23 },
+        { name: 'Khuong', age: 22 }
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button onClick={() => this.swichNameHanler('Hello')}>Swich name</button>
+          <Person click={() => this.swichNameHanler('Hello!')} name="Viet" age="21">
+            My hobbies: Reading Books
+          </Person>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            changed={this.nameChangeHanler}
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+          />
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </header>
+      </div>
+    );
+  }
+
 };
 
-export default app;
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import './Person.css';
 const person = props => {
-  return <div>
-       <p>I'm a {props.name} and I'm { props.age} years old! </p>
-       <p>{props.children}</p>
+  return <div className="Person">
+    <p onClick={props.click}>I'm a {props.name} and I'm {props.age} years old! </p>
+    <p>{props.children}</p>
+    <input type='text' onChange={props.changed} value={props.name} />
   </div>;
 };
 
